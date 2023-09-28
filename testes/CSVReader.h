fcb7ced64039a2fac5526a9bd3c4df905a0e6999
RafaelNogueiraXD <rafa10_nogueira@hotmail.com>
2023-09-28 17:39:01 -0300

    csvReader

---
+++ testes/CSVReader.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+class CSVReader {
+private:
+    std::vector<std::vector<std::string>> data;
+
+public:
+    // Construtor que lê o arquivo CSV
+    CSVReader(const std::string& filename) {
+        std::ifstream file(filename);
+        std::string line;
+        while (getline(file, line)) {
+            std::vector<std::string> row;
+            std::stringstream ss(line);
+            std::string cell;
+            while (getline(ss, cell, ';')) { // Usando ';' como delimitador
+                row.push_back(cell);
+            }
+            data.push_back(row);
+        }
+    }
+
+    // Método para retornar os dados em uma matriz
+    std::vector<std::vector<std::string>> getData() const {
+        return data;
+    }
+};
